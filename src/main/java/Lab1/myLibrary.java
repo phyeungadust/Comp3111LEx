ae9783b9034df1dc611ecac84f3a1ea6ad506a50
phyeungadust <phyeungad@ust.hk>
2024-02-07 17:29:26 +0800

    This is an initial commit for Comp3111Ex Project

---
+++ src/main/java/Lab1/myLibrary.java
@@ -0,0 +1,21 @@
+package Lab1;
+
+public class myLibrary {
+
+    public static int power(int base, int exponent) {
+        if (exponent == 1) {
+            return base * exponent;
+        } else {
+            return base * power(base, exponent - 1);
+        }
+    }
+
+    public static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+}
