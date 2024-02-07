38ff92512092da070fc2206ec7907a1404c74105
phyeungadust <phyeungad@ust.hk>
2024-02-07 18:21:13 +0800

    Add 2 print lines as report heading

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -5,6 +5,8 @@ public class mainApp1 {
     public static void main(String[] args) {
         int b = 2;
         int n = 11;
+        System.out.println("Welcome to Scientific Calculator!");
+        System.out.println("Program started ...");
         System.out.println(b + " to power " + n + " = " + myLibrary.power(b, n));
         System.out.println(n + "! = " + myLibrary.factorial(n));
     }
